aa894ac649e2745682bf92bd9e31ec3713a93811
Charlotte Chang <charlottechang@charlottes-mbp.leandog.net>
2014-11-06 09:26:07 -0500

    removed nucleotide_constructor

--- complement.rb
+++ complement.rb
@@ -8,16 +8,11 @@ class Complement
 		rna.tr('GCAU', 'CGTA')
 	end		
 
-	def self.nucleotide_constructor(strand, method)
-		strand.chars.map { |nucleotide| 
-			send(method, nucleotide) }.join
-	end
-
-	def self.of_dna(strand)			
-		self.nucleotide_constructor(strand, :convert_dna)
+	def self.of_dna(strand)	
+		self.convert_dna(strand)
 	end
 
 	def self.of_rna(strand)
-		self.nucleotide_constructor(strand, :convert_rna)
+		self.convert_rna(strand)
 	end
 end
